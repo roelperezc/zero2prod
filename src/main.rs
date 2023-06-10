8da1d55f1cdf97e15411a7b5b38b4df5a9620184
Roel Perez <roelperez.c@gmail.com>
2023-06-09 18:53:22 -0600

    Crea proyecto con de servidor web con base de datos de Postgres
    Crea un proyecto de servidor web de Actix y una base de datos de Postgres. Se puede hacer POST de datos de nombre y email y guardar en la BD.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration()
+        .expect("Failed to read configuration.");
+
+    let address = format!("localhost:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address)
+        .expect("Failed to bind random port");
+
+    run(listener)?.await
 }
