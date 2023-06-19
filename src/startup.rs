c51a3aee2687de2d4f65724985ef1cec21239b36
Roel Perez <roelperez.c@gmail.com>
2023-06-19 14:01:17 -0600

    Agrega request_id a los mensajes de tracing.

--- src/startup.rs
+++ src/startup.rs
@@ -7,6 +7,7 @@ use actix_web::dev::Server;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use actix_web::middleware::Logger;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes::*;
 
@@ -18,6 +19,7 @@ pub fn run(
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .wrap(Logger::default())
             .route("/health_check",web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
