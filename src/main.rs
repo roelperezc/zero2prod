3c2b58e8b720f4ae2628b1ffea206fff3291ddc8
Roel Perez <roelperez.c@gmail.com>
2023-06-20 13:10:05 -0600

    Optimiza la imagen Docker
    Modifica el archivo Dockerfile para optimizar el tamaño y el tiempo
    de compilación de la imagen.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
 use std::net::TcpListener;
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -19,8 +20,10 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration()
         .expect("Failed to read configuration.");
 
-    let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-        .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to create Postgres connection pool.");
 
     let address = format!(
         "{}:{}",
